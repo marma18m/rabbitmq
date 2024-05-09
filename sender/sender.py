8637f1f73e4a204a74a150d53d7eeb182ce9a42c
matm <matm@gft.com>
2024-05-09 16:49:13 +0200

    dev: add receiver script

--- sender/sender.py
+++ sender/sender.py
@@ -1,15 +1,16 @@
 import pika
 
-#RabbitMQ Connection
-connection = pika.BlockingConnection(
-    pika.ConnectionParameters(host='rabbitmq'))
+# RabbitMQ Connection
+connection = pika.BlockingConnection(pika.ConnectionParameters(host='rabbitmq'))
 channel = connection.channel()
 channel.queue_declare(queue='test_queue')
 
 message_to_send = 'Hello RabbitMQ!'
 
-#Send to RabbitMQ
-channel.basic_publish(exchange='', routing_key='test_queue', body=message_to_send)
+# Send to RabbitMQ
+channel.basic_publish(
+    exchange='', routing_key='test_queue', body=message_to_send
+)
 print('Sent: ' + message_to_send)
 
 connection.close()
