01cf4b0bb8006f3400fec2f9ae2ec2647dd0c0fd
matm <matm@gft.com>
2024-05-16 10:39:43 +0200

    dev: add communications image messages

---
+++ model_inference/model_inference.py
@@ -0,0 +1,51 @@
+import pika, sys, os
+from messages.output import test_pb2
+from messages.output import comms_pb2
+
+
+def main():
+    connection = pika.BlockingConnection(
+        pika.ConnectionParameters(host='rabbitmq')
+    )
+    channel = connection.channel()
+
+    ram_volume_queue = 'ram_volume_queue'
+    comms_queue = 'communications_queue'
+
+    channel.queue_declare(queue=comms_queue)
+    channel.queue_declare(queue=ram_volume_queue)
+
+    def model_callback(ch, method, properties, body):
+        print("\nReceived: ")
+        message = comms_pb2.ImageAcquisition()
+        message.ParseFromString(body)
+
+        comms_message = comms_pb2.CommsMessage()
+
+        print("\nbody:\n ", message)
+
+        # Publish the received message to the other queue
+        channel.basic_publish(
+            exchange='',
+            routing_key=ram_volume_queue,
+            body=message.SerializeToString(),
+        )
+        print('Sent message to ' + ram_volume_queue)
+
+    channel.basic_consume(
+        queue=comms_queue, on_message_callback=model_callback, auto_ack=True
+    )
+
+    print(' [*] Waiting for messages. To exit press CTRL+C')
+    channel.start_consuming()
+
+
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        print('Interrupted')
+        try:
+            sys.exit(0)
+        except SystemExit:
+            os._exit(0)
